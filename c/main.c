ea21ac8c60a04f9dff853cab8f45248211a95db1
femshima <49227365+femshima@users.noreply.github.com>
2022-03-11 21:32:34 +0900

    A bit faster code

--- c/main.c
+++ c/main.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
+#include <string.h>
 
 const int MAX = 100000000;
 
-int main(){
-    static bool sieve[MAX+1];
-    static int primes[MAX+1];
+int main() {
+    int SQRT_MAX = (int)sqrt((double)MAX);
+    static bool sieve[MAX + 1];
+    static int primes[MAX + 1];
     puts("start");
-    for(int i=0; i<=MAX; i++) sieve[i]=true;
+    memset(sieve, 1, (MAX + 1) * sizeof(sieve[0]));
     sieve[0] = false;
     sieve[1] = false;
-    for(int i=0; i<=MAX; i++){
-        if(sieve[i]){
-            for(int j = i*i; j<=MAX; j+=i) sieve[j] = false;
+    for (int i = 0; i <= SQRT_MAX; i++) {
+        if (sieve[i]) {
+            for (int j = i * i; j <= MAX; j += i) sieve[j] = false;
         }
     }
-    int pcount=0;
-    for(int i=0; i<=MAX; i++){
-        if(sieve[i]){
-            primes[pcount]=i;
+    int pcount = 0;
+    for (int i = 0; i <= MAX; i++) {
+        if (sieve[i]) {
+            primes[pcount] = i;
             pcount++;
         }
     }
 
-    printf("%d\n", primes[pcount-1]);
+    printf("%d\n", primes[pcount - 1]);
     puts("end");
     return 0;
 }
