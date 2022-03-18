8b8b4389b24ab437d17821b16a3e9dedfb07c3a0
boletales <30789006+boletales@users.noreply.github.com>
2022-03-18 11:40:38 +0900

    added lua

---
+++ lua/main.lua
@@ -0,0 +1,29 @@
+print("start");
+
+local max = 100000000
+local sqrtmax = math.sqrt(max)
+local sieve = {}
+for i=0, max do
+  sieve[i] = true
+end
+sieve[0] = 0
+sieve[1] = 0
+
+for i=2, sqrtmax do
+  if sieve[i] then
+    for j = i*i, max, i do
+      sieve[j] = false
+    end
+  end
+end
+local primes = {}
+local pcount = 0
+for i=2, max do
+  if sieve[i] then
+    primes[pcount] = i
+    pcount = pcount + 1
+  end
+end
+print(primes[pcount-1]);
+
+print("end");
